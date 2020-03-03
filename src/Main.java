91461ddb039a99e17ac1bdd220c03c57506073c7
Alexandr2307 <kim.alexandr.work@gmail.com>
2020-03-03 10:26:08 +0600

    опять все в говне. надо доделать разметку и навести порядок

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        //new Window(args[0], args[1], args[2]).setVisible(true);
-        new Window().setVisible(true);
+        new Window(args[0], args[1], args[2]).setVisible(true);
+        //new Window().setVisible(true);
     }
 }
